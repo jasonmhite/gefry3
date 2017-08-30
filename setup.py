9accf0beb2d6ed886749ce3fc9c7e88828d288cf
Jason Hite <jasonmhite@gmail.com>
2017-08-30 14:04:18 -0400

    Add 2 clause BSD license

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name="gefry",
-    version="3.0.1",
+    version="3.1.1",
     author="Jason M. Hite",
     license="BSD",
     packages=["gefry3"],
     install_requires=['shapely', 'numpy'],
-    # package_data={'gefry2': ['data/*.h5']}
+    license="2-clause BSD (FreeBSD)",
     )                     
