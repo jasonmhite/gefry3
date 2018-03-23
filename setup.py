f52d6af3a0a416b92fe99127a9dd09ae24bdda47
Jason Hite <jasonmhite@gmail.com>
2018-03-23 17:27:37 -0400

    Fix license in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     name="gefry",
     version="3.4.2",
     author="Jason M. Hite",
-    license="BSD",
     packages=["gefry3"],
     install_requires=['shapely', 'numpy'],
     license="2-clause BSD (FreeBSD)",
